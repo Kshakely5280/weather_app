952f6a7912bef9295fcb532376148d1524d49931
Kyle Shakely <kyleshakely@yahoo.com>
2023-01-31 19:16:12 -0700

    saving current progress

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // api key: c1a19879607eb06816098f47fc86a589
 
-`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+
+// let currentWeather = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+// let forcast = http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// let geo = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+// geo code https://api.openweathermap.org/geo/1.0/direct?q=denver&appid=c1a19879607eb06816098f47fc86a589
+
+//Psuedo code:
+
+// I want to open a webpage and input the location so I can see the weather foorcast
+    // form input for user and event listener to submit
+
+//weather to display for location selected
+    // fetch response data weather through api
+    // data fetched should include current weather, icon, temperature, humidity, and wind speed
+
+// need 5 day forcast for that location to display
