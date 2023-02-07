935e91eb668667ab5802c762e2c122bf0a3b8c90
Kyle Shakely <kyleshakely@yahoo.com>
2023-02-06 21:13:20 -0700

    working current weather, but has preventdefault issue

--- script.js
+++ script.js
@@ -1,115 +1,39 @@
+// const apiKey = "c1a19879607eb06816098f47fc86a589" 
 
- const apiKey = "c1a19879607eb06816098f47fc86a589";
-// const city = "denver";
+// const searchBtn = document.querySelector(".btn");
+// const citySelection = document.querySelector('#city');
+// const currentCity = document.querySelector('.card-title');
+// const currentTemp = document.querySelector('.temp');
+// const currentWind = document.querySelector('.wind');
+// const currentHumid = document.querySelector('.humid');
 
 
-function getWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-      .then(response => response.json())
-      .then(data => {
-        let forecastData = "";
-        let forecastDivs = document.querySelectorAll(".forecast-day");
-        for (let i = 0; i < forecastDivs.length; i++) {
-          let iconCode = data.list[i * 8].weather[0].icon;
-          let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-          forecastDivs[i].innerHTML = `
-            <p>Day ${i + 1}: Low - ${data.list[i * 8].main.temp_min}°F, High - ${data.list[i * 8].main.temp_max}°F</p>
-            <p>Icon: <img src="${iconUrl}" alt="Weather Icon"></p>
-          `;
-        }
-      })
-      .catch(error => console.error(error));
-  }
-  
-  document.querySelector("#search-form").addEventListener("submit", e => {
-    e.preventDefault();
-    let city = document.querySelector("#city-input").value;
-    getWeather(city);
-  });
 
-// fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-//   .then(response => response.json())
-//   .then(data => {
-//     let forecastData = "";
-//     let forecastDivs = document.querySelectorAll(".forecast-day");
-//     for (let i = 0; i < forecastDivs.length; i++) {
-//       let iconCode = data.list[i * 8].weather[0].icon;
-//       let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-//       forecastDivs[i].innerHTML = `
-//         <p>Day ${i + 1}: Low - ${data.list[i * 8].main.temp_min}°F, High - ${data.list[i * 8].main.temp_max}°F</p>
-//         <p>Icon: <img src="${iconUrl}" alt="Weather Icon"></p>
-//       `;
-//     }
-//   })
-//   .catch(error => console.error(error));
+// searchBtn.addEventListener ("click", function(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+citySelection.value+'&appid=c1a19879607eb06816098f47fc86a589&units=imperial')
+//     .then(res=>res.json())
+//     .then(data=>{
+//         console.log(data);
+//         currentCity.textContent=data.name
+//         currentTemp.textContent=`Temp: ${data.main.temp.toFixed(0)}°F`
+//         currentWind.textContent=`Wind: ${data.wind.speed} mph`
+//         currentHumid.textContent=`Humidity: ${data.main.humidity}%`
+//         currentCity.textContent=`Currently in: ${data.name}`
+//         event.preventDefault()
+        
 
 
 
 
 
-
-
-
-
-
-// api key: c1a19879607eb06816098f47fc86a589
-
-// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-
-// let currentWeather = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-// let forcast = http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-// let geo = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// geo code https://api.openweathermap.org/geo/1.0/direct?q=denver&appid=c1a19879607eb06816098f47fc86a589
-
-//Psuedo code:
-
-// I want to open a webpage and input the location so I can see the weather foorcast
-    // form input for user and event listener to submit
-
-//weather to display for location selected
-    // fetch response data weather through api
-    // data fetched should include current weather, icon, temperature, humidity, and wind speed
-
-// need 5 day forcast for that location to display
-
-// function getWeather(){
-//     const newName = document.getElementById("cityInput")
-//     const cityName = document.getElementById("cityName")
-//     cityName.innerHTML = newName.value
-
-// fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=c1a19879607eb06816098f47fc86a589")
-// .then(response => response.json())
-// .then(data =>{
-//     for (let i = 0; i < 5; i++) {
-//        document.getElementById("day" + (i++) + "low").innerHTML = "low" + Number(data.list[i].main.temp_min -288.53).toFixed(0)+"°";     
-//     }
-//     for (let i = 0; i < 5; i++) {
-//         document.getElementById("day" + (i++) + "high").innerHTML = "high" + Number(data.list[i].main.temp_max -288.53).toFixed(0)+"°";     
-//      }
-//      for (let i = 0; i < 5; i++) {
-//         document.getElementById("img" +(i++)).src="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
-//      }
+//         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`)
+//         .then(res=>res.json())
+//         .then(data=>{
+//         console.log(data);
+//         currentCity.textContent=data.name
+//         console.log(data)
+//     })
+// }) 
 // })
 
-// .catch(err => alert("Something went wrong, please try another city"))
-// }
 
-
-
-// function getWeather(){
-// let city = document.getElementById(cityInput)
-// let apiKey = "c1a19879607eb06816098f47fc86a589"
-// city.innerHTML = city.value
-// fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-//   .then(response => response.json())
-//   .then(data => {
-//     for (let i = 0; i < data.list.length; i += 8) {
-//       console.log(`Day ${i / 8 + 1}: Low - ${data.list[i].main.temp_min}°F, High - ${data.list[i].main.temp_max}°F`);
-//     }
-//   })
-//   .catch(error => console.error("Something went wrong, please try another city"));
-
-// }
